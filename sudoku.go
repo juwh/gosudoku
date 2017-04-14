889646f06db92cb2400db231c91639701433c039
William H. Ju <william.h.ju@vanderbilt.edu>
2017-04-13 21:41:15 -0500

    File reader

--- sudoku.go
+++ sudoku.go
@@ -1,7 +1,32 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strings"
+	"strconv"
+)
 
 func main() {
-	fmt.Println("Hello, 世界")
+	const max_dim = 9
+	const box_dim = 3
+	var board[9][9]int
+
+	file, err := os.Open("sudoku-test1.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		row := strings.Split(scanner.Text(), " ")
+		fmt.Println(row[0] + row[1])
+	}
+
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
 }
