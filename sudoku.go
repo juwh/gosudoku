b89e1db92f6a72917795913104837cfd3b9c42b9
nicolekim <nicole.e.kim@vanderbilt.edu>
2017-04-13 21:46:44 -0500

    Started check funcs

--- sudoku.go
+++ sudoku.go
@@ -9,10 +9,38 @@ import (
 	"strconv"
 )
 
+const max_dim = 9
+const box_dim = 3
+var board[9][9]int
+
+func checkRow (row int, num int) bool {
+	for i:=0; i < 9; i++ {
+		if board[row][i] == num {
+			return false
+		}
+	}
+	return true
+}
+
+func checkCol (col int, num int) bool {
+	for i:=0; i < 9; i++ {
+		if board[i][col] == num {
+			return false
+		}
+	}
+	return true
+}
+
+func checkSquare (row int, col int, num int) bool {
+	rowStart :=
+}
+
+func check(row int, col int, num int) bool {
+}
+
+
 func main() {
-	const max_dim = 9
-	const box_dim = 3
-	var board[9][9]int
+
 
 	file, err := os.Open("sudoku-test1.txt")
 	if err != nil {
