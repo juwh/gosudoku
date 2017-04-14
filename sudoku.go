d01932df5b907ef24bfa2cee7bb42f0ba3f0d201
William H. Ju <william.h.ju@vanderbilt.edu>
2017-04-13 22:05:03 -0500

    collab merge

--- sudoku.go
+++ sudoku.go
@@ -11,7 +11,36 @@ import (
 
 const max_dim = 9
 const box_dim = 3
+const filePath = "sudoku-test1.txt"
 var board[9][9]int
+
+func readFile (filePath string) {
+	file, err := os.Open(filePath)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		rowNum := 0
+		row := strings.Split(scanner.Text(), " ")
+		// index and value
+		for i, v := range row {
+			board[rowNum][i], err = strconv.Atoi(v)
+			if err != nil {
+				// handle error
+				fmt.Println(err)
+				os.Exit(2)
+			}
+		}
+		rowNum++
+	}
+
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
+}
 
 func checkRow (row int, num int) bool {
 	for i:=0; i < 9; i++ {
@@ -49,23 +78,6 @@ func check(row int, col int, num int) bool {
 	return checkRow(row,num) && checkCol(col,num) && checkSquare(row,col,num)
 }
 
-
 func main() {
-
-
-	file, err := os.Open("sudoku-test1.txt")
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer file.Close()
-
-	scanner := bufio.NewScanner(file)
-	for scanner.Scan() {
-		row := strings.Split(scanner.Text(), " ")
-		fmt.Println(row[0] + row[1])
-	}
-
-	if err := scanner.Err(); err != nil {
-		log.Fatal(err)
-	}
+	readFile(filePath)
 }
